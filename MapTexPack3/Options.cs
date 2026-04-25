9c49c026a7c94d4553964f9d24df3aa5a99d53f1
GompDS <79070939+GompDS@users.noreply.github.com>
2026-04-25 02:52:47 -0700

    remove license notices from MapTexPack3 files

--- MapTexPack3/Options.cs
+++ MapTexPack3/Options.cs
@@ -1,22 +1,3 @@
-//    MapTexPack3
-//    A console app that primarily automates the process of packing dds
-//    textures into a Dark Souls 3 TPFBDT.
-//
-//    Copyright (C) 2026  GompDS
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License,
-//    any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 using System.Text.RegularExpressions;
 
 namespace MapTexPack3;
